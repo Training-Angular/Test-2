0c034b253ad20287e7a18d4c0f7358f9ab14508e
JokerV <Quocvietcoi2000@gmail.com>
2019-11-08 21:31:13 +0700

    Test 2 - Messaging

---
+++ js/index.js
@@ -0,0 +1,42 @@
+function toggle() {
+  var navRes = document.getElementById('nav-responsive');
+  var sidebarRes = document.getElementById('sidebar-responsive');
+  if (navRes.className === 'nav' && sidebarRes.className === 'sidebar') {
+    navRes.className += ' responsive';
+    sidebarRes.className += ' responsive';
+  } else {
+    navRes.className = 'nav';
+    sidebarRes.className = 'sidebar';
+  }
+}
+
+var iconToggle = document.getElementById('icon-toggle');
+iconToggle.onclick = toggle;
+
+// Show detail message
+
+function showDetailMessage() {
+  var widthScreen = screen.width;
+  var chatRightbar = document.getElementById('chat-rightbar');
+  var chatLeftbar = document.getElementById('chat-leftbar');
+  if (widthScreen <= 768) {
+    chatRightbar.style.display = 'block';
+    chatLeftbar.style.display = 'none';
+  }
+}
+
+var chatTabs = document.querySelectorAll('.chat-tab');
+chatTabs.forEach(chatTab => {
+  chatTab.onclick = showDetailMessage;
+});
+
+function backListChats() {
+  var chatRightbar = document.getElementById('chat-rightbar');
+  var chatLeftbar = document.getElementById('chat-leftbar');
+
+  chatRightbar.style.display = 'none';
+  chatLeftbar.style.display = 'block';
+}
+
+var btnBack = document.getElementById('btn-back');
+btnBack.onclick = backListChats;
